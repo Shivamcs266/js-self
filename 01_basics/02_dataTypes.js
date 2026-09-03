02136e1e81aabc4792f8016bdcbd88a61d1c9c21
Shivamcs266 <shivametw266@gmail.com>
2026-09-04 00:36:29 +0530

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as a newer version.
+
+// alert( 3 + 3) we are using node js not browser
+
+let name = "shivam"
+let age = 24
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint => for large data.
+// string => "Shivam"
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "shivam"); //string
+
+console.log(typeof age);  //number
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); //undefined
