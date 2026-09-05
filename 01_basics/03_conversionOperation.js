f2337177ee5fb8241f699cfc138870f18bbe8857
Shivamcs266 <shivametw266@gmail.com>
2026-09-05 23:30:57 +0530

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "node 01_basics/03_conversionOperation.jshitesh"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33" => NaN
+// true => 1; false => 0;
+
+
+let isLoggedIn = 1
+
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false; "shivam" => true;
+
+let someNumber = 33
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
