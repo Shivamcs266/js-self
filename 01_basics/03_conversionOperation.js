66a8f8fc5e96b9673295049378995cc764934b89
Shivamcs266 <shivametw266@gmail.com>
2026-09-06 00:06:32 +0530

    operation of dataTypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,5 +25,39 @@ let isLoggedIn = 1
 let someNumber = 33
 
 let stringNumber = String (someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *************************** Operation ***************************
+
+let value = 3
+let negValue =-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "shivam"
+let str2 = " sharma"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);        // if the first value is string then no any conversion.
+// console.log(1 + 2 + "2");        // if the string is end then first value perform conversion.
+
+
+// console.log(true);
+// console.log(+true);
+
+let gameCounter = 100
+gameCounter++;            // prefix & postfix conversion => learn mdn 
+console.log(gameCounter);
+
+
