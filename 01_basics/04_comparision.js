ca3dd4ca9b2aa276005ae5a8662effb3131f2f7a
Shivamcs266 <shivametw266@gmail.com>
2026-09-09 12:54:32 +0530

    comparison of datatypes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+/* console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1); */
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+//******************************** */  Most cases we are avoid these type of comparision/conversion.
+
+
+console.log(null > 0);               //comparision convert null to a number, treating it as 0; That's why (3) null >= o is true,(1) null > 0 is false;
+console.log(null == 0);
+console.log(null >= 0);
+
+//*************************** */        Most cases we are avoid these type of comparision/conversion.
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
