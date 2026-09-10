737984caa062396b79a695c0b1c8c8d004683460
Shivamcs266 <shivametw266@gmail.com>
2026-09-10 11:05:33 +0530

    Stack & Heap Memory

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -11,7 +11,7 @@ let userEmail; // undefined
 
 const id = Symbol('123')                 //Symbol
 const anotherId = Symbol('123')
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
 //const bigNumber = 485993849393894n  //bigInt
 
@@ -31,12 +31,34 @@ const myFunction = function(){
     console.log("Hello World");
 }
 
-console.table([score, scoreValue, isLoggedIn, outsideTemp, userEmail] );
+//console.table([score, scoreValue, isLoggedIn, outsideTemp, userEmail] );
 
+/*
 console.log(typeof outsideTemp);
 console.log(typeof isLoggedIn);
 console.log(typeof myFunction);
 console.log(typeof heroes);
+*/
 
+// ********************Memory************************
 
+// Stack (Primitive), Heap (Non Primitive)
+ 
+let myYoutubename = "Shivammax2"    //stack memory example
 
+let anothername = myYoutubename
+anothername = "shivamontrack"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {                //heap memory example 
+    email: "user@google.com",
+    upi: "user@ybl",
+}
+
+let userTwo = userOne
+userTwo.email = "shivam@google"
+
+console.log(userOne.email);
+console.log(userTwo.email);
