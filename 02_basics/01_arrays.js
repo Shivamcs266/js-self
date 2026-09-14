7ad180ad661d63776a6eb63728e33587ff91aabb
Shivamcs266 <shivametw266@gmail.com>
2026-09-14 20:05:19 +0530

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,27 @@
+// Array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["shaktiman", "batman"]
+
+const myArr2 = new Array (1, 2, 3, 4)
+// console.log(myArr[4]);
+
+
+// Array methods
+
+// myArr.push(6)    //to add new value
+// myArr.push(7)
+// myArr.pop()   //to remove last added value
+
+// myArr.unshift(9)  //to add new value in start the array.
+// myArr.shift()     // to remove the value which is add in start the array.
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log(typeof newArr); 
