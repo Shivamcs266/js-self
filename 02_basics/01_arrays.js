4f358cd585a3ceb9418d04e0d346e7581232e6e3
Shivamcs266 <shivametw266@gmail.com>
2026-09-16 20:15:02 +0530

    Array-slice & splice

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -21,7 +21,25 @@ const myArr2 = new Array (1, 2, 3, 4)
 // console.log(myArr.indexOf(9));
 // console.log(myArr.indexOf(3));
 
-const newArr = myArr.join()
+// const newArr = myArr.join()
 
-console.log(myArr);
-console.log(typeof newArr); 
+// console.log(myArr);
+// console.log(typeof newArr); 
+
+
+//********Slice, Splice */
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)  //print slice
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3) //print splice "original array ko manipulate kr deta hai." beech ke part ko gayab kr deta hai jese 1,2,3 ko"
+console.log("C", myArr);
+
+console.log(myn2);
+
+
+
