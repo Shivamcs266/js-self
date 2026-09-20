b9067ec8732dd9c978104711ca746635aa00a08f
Shivamcs266 <shivametw266@gmail.com>
2026-09-20 19:20:46 +0530

    functions with object

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,4 +35,42 @@ function loginUserMessage(username  = "sam"){
     return `${username} just logged in`
 }
 // console.log(loginUserMessage("shivam"));
-console.log(loginUserMessage("Shivam"))
+// console.log(loginUserMessage("Shivam"))
+
+//*************Rest operator**********/
+
+// function calculateCartPrice(...num1){             //rest and spread operator.
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200, 400, 500));
+
+// function calculateCartPrice(val1, val2, ...num1){
+//     return num1
+// }
+// 
+
+const user = {                //object ko function main kese use kiya jata hai.
+    username: "shivam",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+
+// handleObject (user)
+handleObject({             //direct object bhi paas kr skte hai
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 300, 400, 500]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 300, 400, 500]))     //direct function main object se return lena.
